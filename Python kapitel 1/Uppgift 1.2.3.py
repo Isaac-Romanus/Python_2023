9cc85d5dd2fddb7433b552471d0dbe6e98fe65ad
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-01-29 12:33:58 +0100

    Uppdaterat för re packetet, ger endast en position

--- Python kapitel 1/Uppgift 1.2.3.py
+++ Python kapitel 1/Uppgift 1.2.3.py
@@ -1,2 +1,9 @@
 # Uppgift 1.2.3
+import re
+value_str = "1123123" # str(input("Skriv en sträng med återkommande element någonstans"))
+pattern = "123" # str(input"Skriv mönstret i strängen här")
+pattern_pos = re.finditer(pattern, value_str)
 
+print(pattern_pos)
+
+# Re är regular expression.
