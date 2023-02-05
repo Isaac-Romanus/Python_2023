bffbf469d408ad8263ea7a03da2b2eb882e32fbb
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-02-05 19:14:14 +0100

    Klar med Uppgift 1.2.3, löste det med en loop

--- Python kapitel 1/Uppgift 1.2.3.py
+++ Python kapitel 1/Uppgift 1.2.3.py
@@ -1,10 +1,19 @@
 # Uppgift 1.2.3
-import re
-value_str = "123aa123" # str(input("Skriv en sträng med återkommande element någonstans"))
-pattern = "123" # str(input"Skriv mönstret i strängen här")
-pattern_pos = re.search(pattern, value_str)
 
-print(pattern_pos)
+import re
+
+# Tar emot sträng och mönster från användaren
+value_str = str(input("Skriv en sträng med återkommande element någonstans: ")) #"123aa123" - Test sträng
+pattern = str(input("Skriv mönstret i strängen här: "))
+
+# En loop för att hitta mönstret och printa hur många gånger mönstret förekommer samt startposition
+cnt = 0
+for match in re.finditer(pattern, value_str, re.IGNORECASE):
+    cnt += 1
+    if cnt%10 < 3:
+        print(cnt, ":a match start-index: ", match.start())
+    else:
+        print(cnt, ":e match start-index: ", match.start())
 
 # re är regular expression, packet för att leta mönster och
-# re.search specifict letar och ger corresponding "match object"
+# re.finditer specifict letar och ger icke-överlappande "match object" i en lista
