8aa36289d95cef38bafa7b74e140506d9e50d609
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-02-05 21:38:11 +0100

    Startata uppgift 1.4.1

---
+++ Python kapitel 1/Uppgift 1.4.1.py
@@ -0,0 +1,12 @@
+# Uppgift 1.4.1
+
+name_age = input("Ange Namn och ålder separerat av ett \",\"")
+name_age_list = name_age.split(",")
+sum_age = name_age_list[0]
+print(sum_age)
+
+
+
+more = False
+while more != False:
+    sum
