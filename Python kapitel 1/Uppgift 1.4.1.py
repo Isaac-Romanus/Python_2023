5b27e388ed76ac4fef5d5c34d290fbcbecb1e84c
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-02-05 22:04:21 +0100

    Klarat uppgift 1.4.1 om än lite fult

--- Python kapitel 1/Uppgift 1.4.1.py
+++ Python kapitel 1/Uppgift 1.4.1.py
@@ -1,12 +1,32 @@
 # Uppgift 1.4.1
 
-name_age = input("Ange Namn och ålder separerat av ett \",\"")
+# Införskaffande av variabler och namngivning
+name_age = input("Ange Namn och ålder separerat av ett \",\": ")
 name_age_list = name_age.split(",")
-sum_age = name_age_list[0]
-print(sum_age)
+name = name_age_list[0]
+age = int(name_age_list[1])
+sum_age = int(name_age_list[1])
+count = 1
 
-
-
-more = False
+# While-loop för att ange fler individer
+more = True
 while more != False:
-    sum
+    name_age = input("Ange nästa individ på samma sätt som tidigare, eller \"stop\": ")
+    if name_age == "stop":
+        break
+    name_age_list = name_age.split(",")
+    count += 1
+
+    # Kontroll om den senast tillagda personen är äldre än den hittills äldsta
+    if age < int(name_age_list[1]):
+        age = int(name_age_list[1])
+        name = name_age_list[0]
+    
+    # Beräkning av medelåldern i gruppen
+    sum_age += int(name_age_list[1])
+    age_avg = sum_age / count
+
+    # Print för varje iteration av loopen
+    print(f"Antal individer angivna är: {count} \n Deras medelålder är: {age_avg}\n"\
+          f"Äldsta individen är: {name} som är {age} år \n")
+print("Programmet har upphört")
