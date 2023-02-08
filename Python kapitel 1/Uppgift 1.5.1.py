32db30f76eb3f00b827191fdf6f5baae48ea2115
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-02-08 18:24:20 +0100

    Gjorde om till list comprehension variant

--- Python kapitel 1/Uppgift 1.5.1.py
+++ Python kapitel 1/Uppgift 1.5.1.py
@@ -1,16 +1,13 @@
 # Uppgift 1.5.1 
 # (1p): Skriv ett program som skriver ut alla positiva delare till ett positivt heltal.
 
-number = input("Ange ett positivt heltal: ")
-L = []
+number = int(input("Ange ett positivt heltal: "))
 
 # Loop som testar alla nummer från 1 till det angivna numret
-for i in range(1, number+1):
-    if number%i == 0:
-        L.append(i)
+l = [i for i in range(1, number+1) if not number%i]
 
-print(f"De positiva delarna till {number} är: {L}")
-
+print(f"De positiva delarna till {number} är: {l}")
 
 # for i in range(först, sist, steg):
+# List comprehension
 
