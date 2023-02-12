26c28c42aec63479edf06fac399145cb9b8d6555
Isaac <107411530+Isaac-Romanus@users.noreply.github.com>
2023-02-12 09:12:59 +0100

    Add files via upload

---
+++ Python kapitel 1/Uppgift 2.1.1.py
@@ -0,0 +1,17 @@
+# Uppgift 2.1.1 (2p):
+# Skapa en funktion som omvandlar ett heltal (int) till andra talsystem. Funktionen tar in ett heltal och en bas, och skriver sedan ut talet i talsystemet med den basen. Basen kan vara mellan 1-16.
+
+def bas_omskriv(tal, bas):
+    whole = str(tal // bas)
+    remainder = tal%bas
+    l_let = ["A", "B", "C", "D", "E", "F"]
+    
+    if remainder != 0
+
+    return number
+    
+test = bas_omskriv("F",16)
+
+print(test)
+    
+    
