27b439c340b23b0b8f0f929529b414ef66cb218e
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-02-14 15:25:16 +0100

    Updaterat estetik

--- Python kapitel 1/Uppgift 1.9.1.py
+++ Python kapitel 1/Uppgift 1.9.1.py
@@ -1,8 +1,10 @@
 # Uppgift 1.9.1
+# Användaren anger en ordföljd där orden är separerade med komma. Med 
+# hjälp av set, skapa en tupel med de inmatade orden. Orden skall inte upprepas i tupeln.
 
-l = input("Skriv ett antal namn sepaerade av \",\": ")
+l_str = input("Skriv ett antal namn sepaerade av \",\": ")
 
-l_u = tuple(set(l.split(",")))
+l_u = tuple(set(l_str.split(",")))
 print(l_u)
 
 # Fungerar genom att set() inte tar hönsyn till ordning eller dubletter
