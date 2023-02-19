181be0c1aed51b1365d5b045e0b1d568721ad4d7
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-02-19 19:11:12 +0100

    Skapat uppgift 2.4.1

---
+++ Python kapitel 2/Uppgift 2.4.1py.py
@@ -0,0 +1,11 @@
+# Uppgift 2.4.1 - Ej klarad
+
+# (2p): Skriv ett program med ett lämpligt användargränssnitt som en elev kan 
+# använda för att öva multiplikationstabellen mellan 1 och 9. Eleven skall ha möjligheten att 
+# välja svårighetsnivå mellan 1 och 5. Svårighetsnivån 1 innebär att eleven har 1 sekund på sig 
+# att svara o.s.v. Programmet ska kontrollera om svaret inmatat av eleven är rätt och om det är 
+# givet under tiden bestämd av svårighetsnivån. I användargränssnittet finns det en möjlighet att
+# välja antal övningar. Frågor, t.ex. 7  9 = ?, förbereds med hjälp av slumpmässig
+# talgenerator.
+
+# Tips: Använd funktionen time från modulen time för att returnera tiden.
