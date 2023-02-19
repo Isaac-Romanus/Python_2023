915f2120da1e0748fb63ec7c1059db5febcd730f
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-02-19 19:06:40 +0100

    Avklarat uppgift 2.31.

---
+++ Python kapitel 2/Uppgift 2.3.1.py
@@ -0,0 +1,31 @@
+# Uppgift 2.3.1
+
+# (1p): Skapa en funktion som kan anropas med ett variabelt antal argument. 
+# Funktionen ska om ingående parametrar är numeriska returnera summan av de kvadrerade
+# elementen. Om inga argument anges eller någon parameter är icke numerisk, returnerar 
+# funktionen ett meddelande om detta
+
+def sum_kvadrat(*arg):
+    # Check if all variables are numerical:
+    l = []
+    if tuple(l) == arg:
+        print("Inga argument angavs")
+        exit()
+
+    [l.append(i) for i in arg if isinstance(i, (int, float))]
+
+    if tuple(l) != arg:
+        print("Något av argumenten är ej numeriskt")
+        exit()
+
+    # Squaring and adding the arguents together and returning the sum
+    else:
+        sum = 0
+        for i in l:
+            sum += i**2
+        return sum
+
+
+print(sum_kvadrat())
+        
+        
