07cee72c8643c069e28a1b7652cd6c9366fea81f
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-02-20 14:18:52 +0100

    Skapat filerna för 2.6.1

---
+++ Python kapitel 2/Uppgift 2.6.1.py
@@ -0,0 +1,17 @@
+# Uppgift 2.6.1
+
+# (1p): Skapa två funktioner som har filnamn som ett ingående argument. Den 
+# första funktionen lägger till en matris (tvådimensionell lista) till filen och den
+# andra skriver ut matriserna som finns i filen. Matriser skall skrivas ut som formaterade
+# tabeller. Demonstrera användning av funktionerna
+
+def matrix_add(a, b, filename):
+    # The fuction will add a matrix to the given file, wiping the previous content
+
+    open(filename, "w+")
+
+def matrix_print(filename):
+    asf = 0
+
+# Testing the functions
+
