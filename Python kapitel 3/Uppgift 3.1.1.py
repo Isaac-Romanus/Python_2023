651006f6db0d0e7b22e0cd6d49693b4ac65dcb3a
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-02-25 07:44:41 +0100

    Skapat uppgift 3.1.1

---
+++ Python kapitel 3/Uppgift 3.1.1.py
@@ -0,0 +1,8 @@
+# Uppgift 3.1.1 
+# (1p): Skapa en vektor bestående av 200 ekvidistanta punkter från och med 12,
+# till och med 18. Beräkna summan av: 
+ 
+# 1) alla element med jämna index, 
+# 2) alla element med udda index, 
+# 3) alla element vars index är delbart med 10,
+# 4) elementen med indexen 2, 5, 19, 92
