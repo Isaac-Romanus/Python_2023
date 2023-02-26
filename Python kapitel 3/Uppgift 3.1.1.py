77c6aa1bc77646ba37ad28a8404ff20e9d0cefa7
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-02-26 12:08:05 +0100

    uppgift 3.1.1

--- Python kapitel 3/Uppgift 3.1.1.py
+++ Python kapitel 3/Uppgift 3.1.1.py
@@ -5,4 +5,15 @@
 # 1) alla element med jämna index, 
 # 2) alla element med udda index, 
 # 3) alla element vars index är delbart med 10,
-# 4) elementen med indexen 2, 5, 19, 92
+# 4) elementen med indexen 2, 5, 19, 92 (kanske lättast att bara göra en skiva här)
+
+import scipy as sp
+import numpy as np
+
+v = np.linspace(12, 18, 200)
+
+# Sumering av en selection av alla tal i v som har index delbart med 2 (jämna)
+print(sum([i for ind, i in enumerate(v) if not ind%2]))
+
+
+# Enumerate() ger en enumerating objekt vilket är objektet och en counter (index)
