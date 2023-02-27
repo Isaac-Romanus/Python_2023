3e8facf1c66a69f007ce47b4cd895f73e649df29
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-02-27 09:47:49 +0100

    Skapat uppgift 4.2.1

---
+++ Python kapitel 4/Uppgift 4.2.1.py
@@ -0,0 +1,12 @@
+# Uppgift 4.2.1
+
+# (1 p). Skapa en klass Student med tre privata instansvariabler som innehåller 
+# studentens namn, efternamn, och ålder. Objekt av klassen Student behålls i en lista L. Skapa 
+# en meny så att användaren kan 1) Lägga till ett nytt objekt till listan, 2) Ta bort objekt med 
+# visst namn, 3) sortera objekten i listan med namn, efternamn, eller ålder, och 4) skriva ut en 
+# formaterad tabell med information om studenterna.
+
+class Student:
+    def __init__(self) -> None:
+        a = 0
+    
