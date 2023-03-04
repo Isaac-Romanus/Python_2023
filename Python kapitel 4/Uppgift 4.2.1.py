9f8c9d359f79228dc4d423fb2b1ccd42b12c7c26
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-03-04 17:33:12 +0100

    Uppdaterat uppgiften

--- Python kapitel 4/Uppgift 4.2.1.py
+++ Python kapitel 4/Uppgift 4.2.1.py
@@ -8,6 +8,7 @@
 
 class Student:
     L = []
+    Lf = []
     def __init__(self, name, surname, age):
         self._name = name
         self._surname = surname
@@ -18,41 +19,71 @@ class Student:
     def __str__(self):
         return f"{self._name}\t {self._surname}\t {self._age}"
     
-
-
-
     @classmethod
     def adjust_list(cls):
-        slct = int(input("Ange nummer för den operation du önskar genomföra: \n"\
-                         "0 - Lägg till ett nytt objekt till listan\n"\
-                            "1 - Ta bort objekt med ett visst namn\n"\
-                                "2 - Sortera listan\n"\
-                                    "3 - Skriv ut en formaterad tabell av listan"))
-        
-        if slct == 0:
-            student = str(input("Ange ny student med formatet: namn,efternamn,ålder"))
-            Student.L.append(student.split(","))
-        
-        if slct == 1:
-            student = str(input("Ange förnamnet på individen du önskar ta bort: "))
-            for i in Student.L:
-                if getattr(Student.L[i], "_name") == student:
-                    controll = input(f"stämmer det att det är {Student.L[i]} som ska bort? om så skriv: 1")
-                    if controll != 1:
-                        print("Isaac har klantat sig")
-                        break
-                    else:
+        go = True
+        while go == True:
+            slct = int(input("\nAnge nummer för den operation du önskar genomföra: \n"\
+                            "0 - Lägg till ett nytt objekt till listan\n"\
+                                "1 - Ta bort objekt med ett visst namn\n"\
+                                    "2 - Sortera listan\n"\
+                                        "3 - Skriv ut en formaterad tabell av listan\n"\
+                                        ": "))
+            
+            if slct == 0:
+                student = str(input("Ange ny student med formatet: namn,efternamn,ålder: "))
+                student_l = student.split(",")
+                Student.L.append(Student(student_l[0], student_l[1], student_l[2]))
+                Student.Lf.append(student_l)
+                continue
+            
+            if slct == 1:
+                student = str(input("Ange förnamnet på individen du önskar ta bort: "))
+                print(Student.L)
+                for i in range(len(Student.L) -1):
+                    if getattr(Student.L[i], "_name") == student:
                         Student.L.pop(i)
-        
-        if slct == 2:
-
-
+                continue
+                        
+                """for i in range(len(Student.L) - 1):
+                    stud = Student.L[i]
+                    if stud[0] == student:
+                         controll = int(input(f"stämmer det att det är {Student.L[i]} som ska bort? om så skriv 1: "))
+                         if controll != 1:
+                              print("Personen togs inte bort")
+                         else:
+                             Student.L.pop(i)
+                continue"""
+                             
+            if slct == 2:
+                sort = int(input("\nHur önskar du sortera listan? \n"\
+                    "0 - Sortering med Förnamn \n "\
+                        "1 - Sortering med Efternamn  \n "\
+                            "2 - Sortering med Ålder  \n "))
                 
+                if sort == 0:
+                    Student.L = sorted(Student.L, key=lambda x: x._name)
+                
+                if sort == 1:
+                    Student.L = sorted(Student.L, key=lambda x: x._surname)
+                
+                if sort == 2:
+                    Student.L = sorted(Student.L, key=lambda x: x._age)
+                    
+            if slct == 3:
+                
+                    
+            if slct == 4:
+                exit()
+                    
+            go += 1
+            if go == 1000:
+                break
+            
 
-
+Student.adjust_list()
 
 
 
 # Kommer inte bli sann inkapsling utan man kan komma åt det med obj._Student.__name
-    
-
+# Lambda är amonym function
