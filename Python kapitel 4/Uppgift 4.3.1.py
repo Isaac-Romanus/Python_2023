2ada8b811ef2a76e5f49a3a03c2b1b9fff1781fb
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-03-04 19:22:28 +0100

    Klarat uppgift 4.3.1

---
+++ Python kapitel 4/Uppgift 4.3.1.py
@@ -0,0 +1,33 @@
+# Uppgift 4.3.1 
+# (1 p). Skapa en klass Person med fyra instansvariabler: namn, efternamn,
+# id, och ålder. Klassen innehåller även en klassvariabel antal som håller reda på hur många 
+# personer finns. Instansvariabeln id initialiseras med närvarande antal personer. Klassen 
+# innehåller instansmetoden __str__() som presenterar informationen om personen på ett 
+# formaterat sätt. Dessutom innehåller klassen en klassmetod som tar reda på hur många Person
+# objekt som har instansierats. 
+
+class Person: # Create a Class
+    count = 0 # Create an int to hold count of persons created
+    
+    def __init__(self, name, surname, age): # Defining tin instans variables
+        self.name = name
+        self.surname = surname
+        self.age = age
+        
+        Person.count += 1 # Adding one to count for every object created
+        self.id = Person.count # Defining an "id" for "närvarande personer"
+        
+    def __str__(self): # Defining how the class is treated in string definition, ex print
+        print(f"Personen heter {self.name} {self.surname} och är {self.age} år gammal")
+    
+    @classmethod # Creating a method to look up how many "Person" objekts have been instansieted
+    def count_person(cls):
+         print(f"Det har instansierats {Person.count} hittills")
+
+# Creating som test objects
+Person("isaac","Thiria","25") 
+Person("Sofia","Olsson","26")
+Person("Viggo","Thiria","15")
+
+# Testing the classmethod
+Person.count_person()
