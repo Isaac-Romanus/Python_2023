09359b14334a4323be400175e5f5a47a4213eb3e
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-03-04 20:10:19 +0100

    Skapat uppgift 4.5.3

---
+++ Python kapitel 4/Uppgift 4.5.1.py
@@ -0,0 +1,8 @@
+# Uppgift 4.5.3
+
+#(2 p). Skap en klass Matris som presenterar 44 matriser. Matriselementen 
+# lagras i en tvådimensionell lista. Klassen innehåller metoden Det som beräknar determinanten 
+# av matrisen. Klassen innehåller även metoden T som transponerar den nuvarande matrisen, 
+# d.v.s. den skapar inte en ny matris. Klassen har 3 överlagrade operatorer för att summera(+), 
+# subtrahera(-) och matrismultiplicera(@) två matriser. Klassen skall ha en lämplig metod för att 
+# printa ut matrisen
