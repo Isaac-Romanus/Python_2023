fb43ff678dbb83b031c70c707723fa0641c05519
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2023-03-05 10:26:20 +0100

    Skapat uppgift 4.5.1

--- Python kapitel 4/Uppgift 4.5.1.py
+++ Python kapitel 4/Uppgift 4.5.1.py
@@ -1,8 +1,9 @@
-# Uppgift 4.5.3
-
-#(2 p). Skap en klass Matris som presenterar 44 matriser. Matriselementen 
-# lagras i en tvådimensionell lista. Klassen innehåller metoden Det som beräknar determinanten 
-# av matrisen. Klassen innehåller även metoden T som transponerar den nuvarande matrisen, 
-# d.v.s. den skapar inte en ny matris. Klassen har 3 överlagrade operatorer för att summera(+), 
-# subtrahera(-) och matrismultiplicera(@) två matriser. Klassen skall ha en lämplig metod för att 
-# printa ut matrisen
+# Uppgift 4.5.1 (1 p)
+ 
+# Skapa en klass Bråk som representerar matematiska bråk. Klassen har två 
+# instansvariabler: nämnare och täljare. Klassen innehåller fyra överlagrade operatorer för 
+# att summera(+), subtrahera(-), multiplicera(*) och dividera(/) två bråk. Klassen innehåller 
+# även en metod Förkorta() som reducerar bråket så att nämnare och täljare inte har 
+# gemensamma delare utom 1. Förkorta() skall anropas i metoden __init__() så att alla 
+# bråk är i en förkortad form. Klassen skall ha en lämplig implementation __str__ för att printa 
+# ut bråket.
